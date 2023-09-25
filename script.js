10a165f8ef67b4352da17ad0b53f3e92da647113
harryRobinson <127695266+Hazbob@users.noreply.github.com>
2023-09-25 08:28:08 +0100

    fixed bug with the underline on current page

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const navBar = document.querySelector(".nav-bar")
 const portfolioSection = document.querySelector('.sec-section')
 const homeButton = document.querySelector('.home')
 const portfolioButton = document.querySelector('.my-work')
-const homeSection = document.querySelector('#sec1')
+const homeSection = document.querySelector('.first-section')
 const width = navBar.offsetWidth
 let options = {
     root: null,
@@ -11,39 +11,41 @@ let options = {
     threshold: 0.05,
 };
 //Below is for the sticky nav
-const stickyNavLogic = function(entries, observer){
-    entries.forEach((entry) => {
-        if(entry.isIntersecting) {
-            //.fixedElement {
-            //     position:fixed;
-            //     top:0;
-            //     width:100%;
-            //     z-index:100;
-            // }
-            navBar.classList.remove("fixedElement")
-
-
-
-
-        }else{
-
-            navBar.classList.add("fixedElement")
-
-
-        }
-    })
-}
+// const stickyNavLogic = function(entries, observer){
+//     entries.forEach((entry) => {
+//         if(entry.isIntersecting) {
+//             //.fixedElement {
+//             //     position:fixed;
+//             //     top:0;
+//             //     width:100%;
+//             //     z-index:100;
+//             // }
+//             navBar.classList.remove("fixedElement")
+//
+//
+//
+//
+//         }else{
+//
+//             navBar.classList.add("fixedElement")
+//
+//
+//         }
+//     })
+// }
 // let stickyNavObserver = new IntersectionObserver(stickyNavLogic, options);
 
-stickyNavObserver.observe(stickyNavTarget)
+// stickyNavObserver.observe(stickyNavTarget)
 
 const currentPageLogic = function(entries, observer){
     entries.forEach(entry =>{
+        console.log(entry)
         if(entry.isIntersecting){
             if(entry.target === portfolioSection){
                 portfolioButton.classList.add('underline')
             }
             if(entry.target === homeSection){
+                console.log('entry is the home section')
                homeButton.classList.add('underline')
             }
         }else{
@@ -58,7 +60,7 @@ const currentPageLogic = function(entries, observer){
 const optionsForCurrentPage = {
     root: null,
     rootMargin: "0px",
-    threshold: 0.9,
+    threshold: 0.5,
 }
 
 const currentPageObserver = new IntersectionObserver(currentPageLogic, optionsForCurrentPage)
